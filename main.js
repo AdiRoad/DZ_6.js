af7f2f0bbb8c001915dc274db31bb74aa45e617a
ADI_ROAD7 <adiroad946@gmail.com>
2022-10-21 04:13:42 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+// Задача 1
+
+// const Array = ['apple','grapes','pineapple','pear','banana'];
+
+// const elem1 = Array[0];
+// const elem2 = Array[1];
+// const elem3 = Array[2];
+// const arr = Array.slice(3)
+
+// console.log(elem1,elem2,elem3,arr);
+
+// Задача 2
+
+// const myArray = ['Шапка','Куртка','Джинсы','Обувь','Перчатки'];
+// myArray.reverse()
+// const element1 =  myArray[0];
+// const element2 = myArray[1];
+// const arr2 = myArray.slice(2)
+
+// console.log(element1,element2,arr2);
+
+// Задача 3
+
+// let name = prompt('change nam');
+
+// function cheker () {
+//     if(name.trim() === ''){
+//         alert('hello' + " " + 'anonim')
+//     }else{
+//         alert('hello' + " " + name)
+//     }
+// }
+// cheker()
